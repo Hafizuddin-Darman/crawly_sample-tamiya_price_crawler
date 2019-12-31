7afe2ccc8352a6a3009eebdbba5adf1619f6c0e4
Hafizuddin-Darman <46783310+Hafizuddin-Darman@users.noreply.github.com>
2019-12-31 02:45:08 -0500

    commit

---
+++ lib/tamiya_item_crawler.ex
@@ -0,0 +1,18 @@
+defmodule TamiyaItemCrawler do
+  @moduledoc """
+  Documentation for TamiyaItemCrawler.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TamiyaItemCrawler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tamiya_item_crawler_test.exs
@@ -0,0 +1,8 @@
+defmodule TamiyaItemCrawlerTest do
+  use ExUnit.Case
+  doctest TamiyaItemCrawler
+
+  test "greets the world" do
+    assert TamiyaItemCrawler.hello() == :world
+  end
+end
